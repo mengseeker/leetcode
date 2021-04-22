8d7b90486e8cbb1c4e5cd3e0922fa229289e92b2
biao <biao.xing@nexttao.com>
2021-04-22 16:37:20 +0800

    init

---
+++ codes/rotate_array_test.go
@@ -0,0 +1,15 @@
+package codes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRotateArray(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	expected := []int{5, 6, 7, 1, 2, 3, 4}
+	Rotate(nums, 3)
+	if !reflect.DeepEqual(nums, expected) {
+		t.Errorf("Expected %#v, Actual: %#v", expected, nums)
+	}
+}
